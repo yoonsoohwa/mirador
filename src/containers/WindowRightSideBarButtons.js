dc6191590ee42e507d8ca28cdc97ef0738ecd41b
윤수화 <blue.yoonsh01@pinnode.com>
2025-04-17 21:27:19 +0900

    오른쪽바 탭 누르면 해당되는 panel 오픈 구현

--- src/containers/WindowRightSideBarButtons.js
+++ src/containers/WindowRightSideBarButtons.js
@@ -23,12 +23,20 @@ import { WindowRightSideBarButtons } from '../components/WindowRightSideBarButto
  */
 const mapDispatchToProps = (dispatch, { windowId }) => ({
   addCompanionWindow: (content) =>
-    dispatch(actions.addOrUpdateCompanionWindow(windowId, { content, position: 'left' })),
+    dispatch(
+      actions.addOrUpdateCompanionWindow(windowId, {
+        content,
+        position: 'right',
+      })
+    ),
 });
 
 /** */
 function hasLayers(canvases, getMiradorCanvas) {
-  return canvases && canvases.some((c) => getMiradorCanvas(c).imageResources.length > 1);
+  return (
+    canvases &&
+    canvases.some((c) => getMiradorCanvas(c).imageResources.length > 1)
+  );
 }
 
 /** */
@@ -38,7 +46,10 @@ function hasAnnotations(canvases, getMiradorCanvas) {
     canvases.some((c) => {
       const canvas = getMiradorCanvas(c);
 
-      return canvas.annotationListUris.length > 0 || canvas.canvasAnnotationPages.length > 0;
+      return (
+        canvas.annotationListUris.length > 0 ||
+        canvas.canvasAnnotationPages.length > 0
+      );
     })
   );
 }
@@ -46,7 +57,10 @@ function hasAnnotations(canvases, getMiradorCanvas) {
 /** */
 function hasSearchResults(state, { windowId }) {
   const { suggestedSearches } = getWindow(state, { windowId });
-  const companionWindowId = getCompanionWindowsForPosition(state, { position: 'left', windowId })?.[0]?.id;
+  const companionWindowId = getCompanionWindowsForPosition(state, {
+    position: 'left',
+    windowId,
+  })?.[0]?.id;
   const searchQuery = getSearchQuery(state, { companionWindowId, windowId });
 
   return Boolean(suggestedSearches || searchQuery);
@@ -58,16 +72,32 @@ function hasSearchResults(state, { windowId }) {
  * @private
  */
 const mapStateToProps = (state, { windowId }) => ({
-  hasAnnotations: getAnnotationResourcesByMotivation(state, { windowId }).length > 0,
-  hasAnyAnnotations: hasAnnotations(getCanvases(state, { windowId }), getMiradorCanvasWrapper(state)),
-  hasAnyLayers: hasLayers(getCanvases(state, { windowId }), getMiradorCanvasWrapper(state)),
-  hasCurrentLayers: hasLayers(getVisibleCanvases(state, { windowId }), getMiradorCanvasWrapper(state)),
+  hasAnnotations:
+    getAnnotationResourcesByMotivation(state, { windowId }).length > 0,
+  hasAnyAnnotations: hasAnnotations(
+    getCanvases(state, { windowId }),
+    getMiradorCanvasWrapper(state)
+  ),
+  hasAnyLayers: hasLayers(
+    getCanvases(state, { windowId }),
+    getMiradorCanvasWrapper(state)
+  ),
+  hasCurrentLayers: hasLayers(
+    getVisibleCanvases(state, { windowId }),
+    getMiradorCanvasWrapper(state)
+  ),
   hasSearchResults: hasSearchResults(state, { windowId }),
   hasSearchService: getManifestSearchService(state, { windowId }) !== null,
   panels: getWindowConfig(state, { windowId }).rightPanels,
-  rightSideBarPanel: (getCompanionWindowsForPosition(state, { position: 'left', windowId })[0] || {}).content,
+  rightSideBarPanel: (
+    getCompanionWindowsForPosition(state, { position: 'right', windowId })[0] ||
+    {}
+  ).content,
 });
 
-const enhance = compose(connect(mapStateToProps, mapDispatchToProps), withPlugins('WindowRightSideBarButtons'));
+const enhance = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withPlugins('WindowRightSideBarButtons')
+);
 
 export default enhance(WindowRightSideBarButtons);
